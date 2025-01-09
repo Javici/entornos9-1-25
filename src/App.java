57851b52c5589b7d64544bf0ee836eaa0e6b7c14
PCJAVIERL\Usuario <javier9913@gmail.com>
2025-01-09 13:56:43 +0100

    no message

--- src/App.java
+++ src/App.java
@@ -1,5 +1,14 @@
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
+
+        coche ferrari= new coche();
+
+        System.out.println(ferrari);
+
+        ferrari= null;
+
+        System.out.println(ferrari);
+
     }
 }
--- src/coche.java
+++ src/coche.java
@@ -1,4 +1,13 @@
 public class coche {
 
+        String color;
+        int anyo
+        String modelo;
+        String marca;
+
+    public coche (){
+        System.out.println("dentro del constructor del coche");
+    }
     
+
 }
